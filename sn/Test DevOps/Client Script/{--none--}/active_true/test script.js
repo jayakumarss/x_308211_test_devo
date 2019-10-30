84c0759c83da6ef0d6dd58d704859c54d3324f9d
System Administrator <admin@example.com>
2019-10-30 14:44:07 -0400

    Changes by 'System Administrator' on 'Test DevOps - 1.0.0' -- Build #1

---
+++ sn/Test DevOps/Client Script/{--none--}/active_true/test script.js
@@ -0,0 +1,10 @@
+/* 
+ * Application : Test DevOps
+ * ClassName   : sys_script_client
+ * Created On  : 2019-10-30 18:41:53
+ * Created By  : admin
+ * Updated On  : 2019-10-30 18:41:53
+ * Updated By  : admin
+ * URL         : /sys_script_client.do?sys_id=a946cf75db340010343f58b3ca961904
+ */
+console.log("test");
---
+++ sn/Test DevOps/Script Include/active_true/x_308211_test_devo.Test Devops.js
@@ -0,0 +1,10 @@
+/* 
+ * Application : Test DevOps
+ * ClassName   : sys_script_include
+ * Created On  : 2019-10-30 18:43:20
+ * Created By  : admin
+ * Updated On  : 2019-10-30 18:43:20
+ * Updated By  : admin
+ * URL         : /sys_script_include.do?sys_id=5ea687b5db340010343f58b3ca961993
+ */
+gs.info('Test');
